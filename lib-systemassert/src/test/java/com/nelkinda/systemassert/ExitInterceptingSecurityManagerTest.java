3198c63183a53d470881784405d705615f81f569
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-06-15 00:29:28 +0530

    Microtest that the exit status is stored in the ExitException.

--- lib-systemassert/src/test/java/com/nelkinda/systemassert/ExitInterceptingSecurityManagerTest.java
+++ lib-systemassert/src/test/java/com/nelkinda/systemassert/ExitInterceptingSecurityManagerTest.java
@@ -2,12 +2,15 @@ package com.nelkinda.systemassert;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExitInterceptingSecurityManagerTest {
+    @SuppressWarnings("PMD.JUnitTestContainsTooManyAsserts")
     @Test
     void checkExitThrowsExitException() {
         final ExitInterceptingSecurityManager securityManager = new ExitInterceptingSecurityManager();
-        assertThrows(ExitException.class, () -> securityManager.checkExit(0));
+        final ExitException exitException = assertThrows(ExitException.class, () -> securityManager.checkExit(42));
+        assertEquals(42, exitException.getStatus());
     }
 }
